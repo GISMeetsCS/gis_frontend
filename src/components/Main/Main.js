8244b7b344b5bf0fe98c02b176e2ea231a414c6d
Nayeon Kim <sksldi0@gmail.com>
2023-08-22 16:12:00 -0500

    file upload

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,13 +4,24 @@ import GoogleMap from '../GoogleMap/GoogleMap';
 import { Button } from 'react-bootstrap';
 
 function Main () {
+    const fileInput = React.useRef(null);
+
+    const inputRef = e => {
+        fileInput.current.click();
+    }
+
+    const mainImgInput_onChange = e => {
+        
+    }
+
     return (
         <div className='Main-box'>
             <div className="Main-main">
                 <form>
                     <input class="form-control" type="search" placeholder="latitude" aria-label="latitude" />
                     <input class="form-control" type="search" placeholder="longitude" aria-label="longitude" />
-                    <Button as="input" type="submit" value="Upload" />
+                    <input accept="image/*" type="file" ref={fileInput} onChange={mainImgInput_onChange} style={{ display: "none" }}/>
+                    <Button onClick={inputRef}>Upload</Button>
                 </form>
             </div>
             <div className="Main-map">
